9d5289ce3de8927fc44557ac83f719428fef038d
dmlyczynski <52506521+dmlyczynski@users.noreply.github.com>
2024-01-08 14:04:41 +0100

    Add signalR client implementation

---
+++ src/DiskMonitoring.Client.Infrastructure/SignalR/MessageHubProxy.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using Polly;
+
+namespace DiskMonitoring.Client.Infrastructure.SignalR;
+
+public interface IMessageHubProxy
+{
+    Task SendReport(string message);
+    Task InitiateConenction();
+    Task Dispose();
+}
+
+public class MessageHubProxy : IMessageHubProxy
+{
+    private readonly HubConnection _hubConnection;
+    private readonly IAsyncPolicy _retryPolicy;
+    private readonly IAsyncPolicy _retryPolicySendReport;
+    private readonly ILogger<MessageHubProxy> _logger;
+
+    public MessageHubProxy(ILogger<MessageHubProxy> logger)
+    {
+        _logger = logger;
+        _hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/reports")
+                .WithAutomaticReconnect()
+                .Build();
+
+        _retryPolicy = Policy
+           .Handle<Exception>()
+           .Or<TaskCanceledException>()
+           .Or<InvalidOperationException>()
+           .WaitAndRetryForeverAsync(retryAttempt => TimeSpan.FromSeconds(2), onRetry: (_, _, _) => { _logger.LogError("Error connecting to hub. Retrying ..."); })
+           ;
+
+        _retryPolicySendReport = Policy
+           .Handle<InvalidOperationException>()
+           .Or<TaskCanceledException>()
+           .WaitAndRetryForeverAsync(retryAttempt => TimeSpan.FromSeconds(2), onRetry: async (_, _, _) => await InitiateConenction());
+        ;
+    }
+
+    public async Task InitiateConenction()
+    {
+        await _retryPolicy.ExecuteAsync(async () =>
+        {
+            if (_hubConnection.State == HubConnectionState.Disconnected)
+            {
+                await _hubConnection.StartAsync();
+            }
+        });
+    }
+
+    public async Task SendReport(string message)
+    {
+        await _retryPolicySendReport.ExecuteAsync(() => _hubConnection.InvokeAsync("SendReport", message));
+
+        _logger.LogInformation("Report sent");
+    }
+
+    public async Task Dispose()
+    {
+        await _hubConnection.DisposeAsync();
+    }
+}
