1d91e0220ed4e33107f77d29a27a0f8d589841e7
dmlyczynski <52506521+dmlyczynski@users.noreply.github.com>
2024-01-08 14:14:02 +0100

    Add hosted service to send reports every hour

--- src/DiskMonitoring.Client.Module/Program.cs
+++ src/DiskMonitoring.Client.Module/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Client;
+using DiskMonitoring.Client.Infrastructure.SignalR;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/log.txt",
+    rollingInterval: RollingInterval.Day,
+    rollOnFileSizeLimit: true)
+    .CreateLogger();
+
+try
+{
+    await Host.CreateDefaultBuilder(args)
+        .ConfigureLogging(logging =>
+        {
+            logging.AddSerilog();
+        })
+        .ConfigureServices(services =>
+        {
+            services.AddTransient<IMessageHubProxy, MessageHubProxy>();
+            services.AddHostedService<ReportsHostedService>();
+        })
+        .Build()
+        .RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Logger.Error("Error occured", ex);
+}
---
+++ src/DiskMonitoring.Client.Module/ReportsHostedService.cs
@@ -0,0 +1,40 @@
+﻿using DiskMonitoring.Client.Infrastructure.SignalR;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Client;
+
+public class ReportsHostedService : IHostedService
+{
+    private readonly IMessageHubProxy _messageHubProxy;
+    private readonly ILogger<ReportsHostedService> _logger;
+
+    public ReportsHostedService(
+        IMessageHubProxy messageHubProxy,
+        ILogger<ReportsHostedService> logger)
+    {
+        _messageHubProxy = messageHubProxy;
+        _logger = logger;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Starting reports service");
+
+        await _messageHubProxy.InitiateConenction();
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            await _messageHubProxy.SendReport("test"); // todo
+
+            await Task.Delay(TimeSpan.FromSeconds(59)); // todo - add to configuration
+        }
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _messageHubProxy.Dispose();
+
+        _logger.LogInformation($"Stopping reports service");
+    }
+}
