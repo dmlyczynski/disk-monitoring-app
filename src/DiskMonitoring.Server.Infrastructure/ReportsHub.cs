e169b046e43aee7b128ca2ca72da49871df98163
dmlyczynski <52506521+dmlyczynski@users.noreply.github.com>
2024-01-08 13:03:23 +0100

    Add implementation for signalR server

---
+++ src/DiskMonitoring.Server.Infrastructure/ReportsHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace DiskMonitoring.Server.Infrastructure;
+
+public class ReportsHub : Hub
+{
+    private readonly ILogger<ReportsHub> _logger;
+
+    public ReportsHub(ILogger<ReportsHub> logger)
+    {
+        _logger = logger;
+    }
+
+    public void SendReport(string value)
+    {
+        _logger.LogInformation(value);
+    }
+}
--- src/DiskMonitoring.Server.Module/Program.cs
+++ src/DiskMonitoring.Server.Module/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DiskMonitoring.Server.Infrastructure;
+
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/log.txt",
+    rollingInterval: RollingInterval.Day,
+    rollOnFileSizeLimit: true)
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddSerilog();
+builder.Services.AddSignalR();
+builder.Services.AddLogging();
+
+builder.WebHost.UseUrls("http://localhost:5000");
+
+var app = builder.Build();
+
+app.MapHub<ReportsHub>("reports");
+
+app.Run();
